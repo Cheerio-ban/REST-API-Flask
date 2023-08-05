5e78a56d2a1b88755f3889b5fceb40e205686e0d
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-08-05 19:17:57 +0100

    Comments or good measure

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 
 from app import db
 
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False, unique=True)
-    description = db.Column(db.String)
-    completed = db.Column(db.Boolean, default=False)
+class Task(db.Model): # Model representing the task table in the database
+    id = db.Column(db.Integer, primary_key=True) #Id (primary key) column
+    title = db.Column(db.String, nullable=False, unique=True) # title column
+    description = db.Column(db.String) # description column
+    completed = db.Column(db.Boolean, default=False) # completed column
 
     def __repr__(self):
-        return '<Task {}>'.format(self.title)
+        return '<Task {}>'.format(self.title) # Output when being called by print or .format
